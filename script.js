a9282db7c6c6af6f891215a324c81be9d5d7eb2a
acollino <arcollino@gmail.com>
2022-08-05 18:54:06 -0700

    update portfolio page

--- script.js
+++ script.js
@@ -13,6 +13,24 @@ function hideMenu() {
   }, 100);
 }
 
+function addFadeEvents() {
+  let hiddenArray = document.querySelectorAll(".hidden-container");
+  for (let hiddenBox of hiddenArray) {
+    hiddenBox.addEventListener("mouseenter", () => {
+      hiddenBox.classList.remove("rapid-hide");
+      hiddenBox.classList.add("slow-reveal");
+      hiddenBox.classList.toggle("opaque");
+      hiddenBox.previousElementSibling.classList.toggle("transparent");
+    });
+    hiddenBox.addEventListener("mouseleave", () => {
+      hiddenBox.classList.remove("slow-reveal");
+      hiddenBox.classList.add("rapid-hide");
+      hiddenBox.classList.toggle("opaque");
+      hiddenBox.previousElementSibling.classList.toggle("transparent");
+    });
+  }
+}
+
 menuIcon.addEventListener("click", revealMenu);
 
 menuIcon.addEventListener("mouseleave", hideMenu);
@@ -21,4 +39,14 @@ menuBar.addEventListener("mouseleave", hideMenu);
 
 menuBar.addEventListener("mouseenter", function (e) {
   clearTimeout(menuInUseTimer);
+});
+
+addFadeEvents();
+
+document.querySelector(".avatar-container").addEventListener("mouseenter", (evt) => {
+  document.querySelector("#avatar").setAttribute("src", "/assets/8_bit_avatar_eyebrows.png");
+});
+
+document.querySelector(".avatar-container").addEventListener("mouseleave", (evt) => {
+  document.querySelector("#avatar").setAttribute("src", "/assets/8_bit_avatar.png");
 });
